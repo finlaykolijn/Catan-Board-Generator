c92721169800595bc36d276616c7d37b38ed8f8f
finlaykolijn <finlaykolijn@outlook.com>
2025-07-15 20:15:06 -0400

    fix: small edit to tsx files

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Renders the app component into the DOM
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
